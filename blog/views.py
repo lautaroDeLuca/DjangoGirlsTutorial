904292069bbb242fb3c5d0ffa754b5266119c32e
lautaroDeLuca <lautaro.deluca30@gmail.com>
2021-01-13 22:30:05 -0300

    added individual view for posts

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.utils import timezone
 
 def post_list(request):
     posts = Post.objects.filter(datePublished__lte=timezone.now()).order_by('datePublished')
     return render(request, 'blog/post_list.html', {'posts' : posts})
+
+def post_details(request, pk):
+    posts = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_details.html', {'posts' : posts})
